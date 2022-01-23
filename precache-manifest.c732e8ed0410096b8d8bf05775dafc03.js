4c064576910146e31a39cb9bb2ff451b680f4390
Tharik J.H <startharik@gmail.com>
2022-01-24 00:34:47 +0800

    Updates

---
+++ precache-manifest.c732e8ed0410096b8d8bf05775dafc03.js
@@ -0,0 +1,386 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0bde28de0ff84bfb2486a993bdd4933",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05c762e521f9b04b73ba",
+    "url": "/static/css/main.c918a37b.chunk.css"
+  },
+  {
+    "revision": "f1da37725bdf62a74199",
+    "url": "/static/js/2.f6845b63.chunk.js"
+  },
+  {
+    "revision": "9de446982c9c8df2f00b7c4cfd8d498e",
+    "url": "/static/js/2.f6845b63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05c762e521f9b04b73ba",
+    "url": "/static/js/main.4f3a495f.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "ef0bf175099820bd6f03d1363975bbc9",
+    "url": "/static/media/1F3E6.ef0bf175.svg"
+  },
+  {
+    "revision": "85f9fe30440975dcd17c269c684a2a98",
+    "url": "/static/media/1F3EC.85f9fe30.svg"
+  },
+  {
+    "revision": "fcd5f0fd0dbf246147ffd03015da0d52",
+    "url": "/static/media/1F468-200D-1F4BC.fcd5f0fd.svg"
+  },
+  {
+    "revision": "f68c93356d5670151abc4f8e9ba50cd2",
+    "url": "/static/media/1F469-1F3FC-200D-1F373.f68c9335.svg"
+  },
+  {
+    "revision": "bbdbfd2518a486aca1d4836c02f6e5b7",
+    "url": "/static/media/1F469-1F3FC-200D-1F4BC.bbdbfd25.svg"
+  },
+  {
+    "revision": "2938e58421fcd05d47bbb2e4a888eeec",
+    "url": "/static/media/1F481-200D-2642-FE0F.2938e584.svg"
+  },
+  {
+    "revision": "c10105a34a827ef01f949cad0849a9c4",
+    "url": "/static/media/1F630.c10105a3.svg"
+  },
+  {
+    "revision": "eab8b18ba95446d44a4fc14eccbbec34",
+    "url": "/static/media/1F646-1F3FB-200D-2640-FE0F.eab8b18b.svg"
+  },
+  {
+    "revision": "dbed4d4cd2980a1feb9e81add7b67838",
+    "url": "/static/media/1F64B-1F3FB-200D-2642-FE0F.dbed4d4c.svg"
+  },
+  {
+    "revision": "a60b54cca8b792f86d4a104ac52b9ca8",
+    "url": "/static/media/1F64B-1F3FD.a60b54cc.svg"
+  },
+  {
+    "revision": "542d3e4af43abc0fd02618d28bedf70b",
+    "url": "/static/media/1F6AA.542d3e4a.svg"
+  },
+  {
+    "revision": "3e1a02003e7e2d61b9ef8d50c9fb473d",
+    "url": "/static/media/1F7E2_greentick.3e1a0200.svg"
+  },
+  {
+    "revision": "ad9f5c2bc683162d3b01cd040b652584",
+    "url": "/static/media/1F935-1F3FC.ad9f5c2b.svg"
+  },
+  {
+    "revision": "c8b92da7ad6699d16a2c6901ac03589f",
+    "url": "/static/media/1F935.c8b92da7.svg"
+  },
+  {
+    "revision": "f3a561a3edde24e48aea6b832a6c8cf7",
+    "url": "/static/media/Hedi.f3a561a3.JPG"
+  },
+  {
+    "revision": "f115563b054cb0632d5f6558f3fe57e0",
+    "url": "/static/media/The_warren_2.f115563b.JPG"
+  },
+  {
+    "revision": "514398353ee1e01f2d3877f9b08caf6b",
+    "url": "/static/media/all_biz.51439835.jpg"
+  },
+  {
+    "revision": "dd52e470eac30e26aa669d180eeca153",
+    "url": "/static/media/arrow.dd52e470.png"
+  },
+  {
+    "revision": "8da641a44f46e7b9bfcc957dfbcdc2d9",
+    "url": "/static/media/article-top-plain.8da641a4.png"
+  },
+  {
+    "revision": "512b4b46a0bbcd9a5c23d3642cbece80",
+    "url": "/static/media/awareness.512b4b46.png"
+  },
+  {
+    "revision": "345ca7afb8eacadb4463e339d3a977ec",
+    "url": "/static/media/basil.345ca7af.png"
+  },
+  {
+    "revision": "0f772963a2264c7f3b966ec831da4ee9",
+    "url": "/static/media/bike.0f772963.png"
+  },
+  {
+    "revision": "fc099c82826c31d07cb337dfeb5e5209",
+    "url": "/static/media/budget-chair.fc099c82.png"
+  },
+  {
+    "revision": "425d019a1373687025c19dd731caf82c",
+    "url": "/static/media/bunting.425d019a.png"
+  },
+  {
+    "revision": "80cc0a2b87d775062d675d39e7d86b41",
+    "url": "/static/media/carrot.80cc0a2b.png"
+  },
+  {
+    "revision": "3a1bd5b0cb2fe1d41824961832387f5b",
+    "url": "/static/media/cash.3a1bd5b0.mp3"
+  },
+  {
+    "revision": "590788b1ace0077a11e3b3f407305701",
+    "url": "/static/media/cheese.590788b1.png"
+  },
+  {
+    "revision": "39afb83da959e16829bb26a448ea8d0a",
+    "url": "/static/media/chicken.39afb83d.png"
+  },
+  {
+    "revision": "0f7b0fc37338e767987e7ee705bf088f",
+    "url": "/static/media/close-window.0f7b0fc3.png"
+  },
+  {
+    "revision": "9cbc774908d88e502c726dde407c5299",
+    "url": "/static/media/community-hand.9cbc7749.png"
+  },
+  {
+    "revision": "040f0bdf208ef26246216695361e48a5",
+    "url": "/static/media/compost-packaging.040f0bdf.png"
+  },
+  {
+    "revision": "be34daa920dd39d1b68ed7202947b4ea",
+    "url": "/static/media/cross.be34daa9.png"
+  },
+  {
+    "revision": "0d650d311fbbcf257538f2a432f06dbf",
+    "url": "/static/media/customer-hand.0d650d31.png"
+  },
+  {
+    "revision": "9c60c7f183242b405be222daff40ad8f",
+    "url": "/static/media/darker-button.9c60c7f1.png"
+  },
+  {
+    "revision": "2788f05dd9998bb04485000276c3ca58",
+    "url": "/static/media/down-joy.2788f05d.png"
+  },
+  {
+    "revision": "9d7fa0b9e66de8b84cfd55a1c01c915a",
+    "url": "/static/media/ethical-hand.9d7fa0b9.png"
+  },
+  {
+    "revision": "508d79fe721fc3d78c13de642c475349",
+    "url": "/static/media/express-delivery.508d79fe.png"
+  },
+  {
+    "revision": "7a2c8f91e3d9ceeb7df13b127e144830",
+    "url": "/static/media/farmhouse.7a2c8f91.png"
+  },
+  {
+    "revision": "4759fa277564f138d54164ca16987ce2",
+    "url": "/static/media/help.4759fa27.png"
+  },
+  {
+    "revision": "e4acdfa29bc71a4c7665e6daddff865a",
+    "url": "/static/media/just-tree-inverse-side.e4acdfa2.png"
+  },
+  {
+    "revision": "9e34dab631f8bbaeceb7e7e90a3f800d",
+    "url": "/static/media/just-tree-side.9e34dab6.png"
+  },
+  {
+    "revision": "611ff7c5c0eb76ca00c03a599ccba4e8",
+    "url": "/static/media/laptop.611ff7c5.png"
+  },
+  {
+    "revision": "23fff60b2539f0315c273942edb81e60",
+    "url": "/static/media/leaf.23fff60b.png"
+  },
+  {
+    "revision": "8c7553aa24e7854fd473122190cacd79",
+    "url": "/static/media/left-joy.8c7553aa.png"
+  },
+  {
+    "revision": "785ea02f3f58d53c738d283d28f346c3",
+    "url": "/static/media/light-button.785ea02f.png"
+  },
+  {
+    "revision": "db512f6f41ba3d29f1e5a75d383aadf1",
+    "url": "/static/media/lisa-head.db512f6f.png"
+  },
+  {
+    "revision": "a099b7c2ff003ac5ec6cfbd0a5c2ed46",
+    "url": "/static/media/luxury-chair.a099b7c2.png"
+  },
+  {
+    "revision": "46acab78fe0d526766f9a36ab430881b",
+    "url": "/static/media/marketing-hand.46acab78.png"
+  },
+  {
+    "revision": "b3ce103bd6915d7bdb0ef48a16af38d6",
+    "url": "/static/media/medium-button.b3ce103b.png"
+  },
+  {
+    "revision": "1fab84469a5f91df1c5c96a3e8417100",
+    "url": "/static/media/menu.1fab8446.png"
+  },
+  {
+    "revision": "784ae1e98741a081eb7385a525cf2e7e",
+    "url": "/static/media/minus.784ae1e9.png"
+  },
+  {
+    "revision": "9d829a7c4db4b0e79f456ddb28667c34",
+    "url": "/static/media/moneydown.9d829a7c.wav"
+  },
+  {
+    "revision": "4a5a906d6e728a0a69af5853021dae89",
+    "url": "/static/media/no-cutlery.4a5a906d.png"
+  },
+  {
+    "revision": "b5cbf9e6bf7233b0c00113824529844e",
+    "url": "/static/media/person-eight.b5cbf9e6.png"
+  },
+  {
+    "revision": "80b5a23ae65770c9ef74acd2350979a2",
+    "url": "/static/media/person-five.80b5a23a.png"
+  },
+  {
+    "revision": "4db26e2d7e662449101736a42bf0c123",
+    "url": "/static/media/person-four.4db26e2d.png"
+  },
+  {
+    "revision": "f40ab3143eef3394920531674324ea98",
+    "url": "/static/media/person-one.f40ab314.png"
+  },
+  {
+    "revision": "d1cf8d6ea70aa0e128a782aad45a9c03",
+    "url": "/static/media/person-seven.d1cf8d6e.png"
+  },
+  {
+    "revision": "4d4acc3e2813cb227e859793f8add53b",
+    "url": "/static/media/person-six.4d4acc3e.png"
+  },
+  {
+    "revision": "eaef72f541fffcbab289624ad9052fc0",
+    "url": "/static/media/person-three.eaef72f5.png"
+  },
+  {
+    "revision": "014494cb83be40ab954ac3e4472e27b1",
+    "url": "/static/media/person-two.014494cb.png"
+  },
+  {
+    "revision": "4c283290f5f29757d83b51e319779b7e",
+    "url": "/static/media/pizzabox.4c283290.png"
+  },
+  {
+    "revision": "2cdec1ac12d1adcfa76565c778f3b1d3",
+    "url": "/static/media/plastic-cutlery.2cdec1ac.png"
+  },
+  {
+    "revision": "ffbda7111d83d9a829e9487fa6f6f6ef",
+    "url": "/static/media/plastic-packaging.ffbda711.png"
+  },
+  {
+    "revision": "e6c21f70a58468235dfb569f6cf3abeb",
+    "url": "/static/media/plus.e6c21f70.png"
+  },
+  {
+    "revision": "848eadf511ce36f6f721820dce26a34c",
+    "url": "/static/media/pork.848eadf5.png"
+  },
+  {
+    "revision": "45b8cf2266057eeec512585b005a1ab8",
+    "url": "/static/media/potato-spade.45b8cf22.png"
+  },
+  {
+    "revision": "4d513b17311d3f65235b0be51411d380",
+    "url": "/static/media/pound-coin.4d513b17.png"
+  },
+  {
+    "revision": "1925c2d8c4a627475ee0e21e54f4dc89",
+    "url": "/static/media/profit-hand.1925c2d8.png"
+  },
+  {
+    "revision": "44cee430ef56f5cf94cb1e2f6d16a584",
+    "url": "/static/media/pylon.44cee430.png"
+  },
+  {
+    "revision": "9fbd349981beb3508207a3ab3d8343db",
+    "url": "/static/media/recycle-packaging.9fbd3499.png"
+  },
+  {
+    "revision": "d269313cb3a6a3aa87a7b2247237e74d",
+    "url": "/static/media/restaurant.d269313c.png"
+  },
+  {
+    "revision": "3c379830fc78699ed4ce24e2d825d2b9",
+    "url": "/static/media/right-joy.3c379830.png"
+  },
+  {
+    "revision": "5c3bf13266bdb32c76fc61b4acc5a3cb",
+    "url": "/static/media/sepoints.5c3bf132.wav"
+  },
+  {
+    "revision": "f3df013c3723e0b03a5d33b6acd2da95",
+    "url": "/static/media/sepointsdown.f3df013c.mp3"
+  },
+  {
+    "revision": "0589df762934810313dcb393d99d6796",
+    "url": "/static/media/social-media.0589df76.png"
+  },
+  {
+    "revision": "167ae6c2e26479be38b7c4d223ff1b04",
+    "url": "/static/media/solar.167ae6c2.png"
+  },
+  {
+    "revision": "395ffd72314f4252a7e45730110b5b36",
+    "url": "/static/media/spade.395ffd72.png"
+  },
+  {
+    "revision": "1f551bd6e2de2d93b63d2aad33f4128b",
+    "url": "/static/media/staff-training.1f551bd6.png"
+  },
+  {
+    "revision": "efdc80f9b2ab95476c40142b2bf7ffae",
+    "url": "/static/media/suzanna.efdc80f9.jpg"
+  },
+  {
+    "revision": "c36bd8130a3b0e6280977c3c2e628bb4",
+    "url": "/static/media/tick.c36bd813.png"
+  },
+  {
+    "revision": "2483fde6c1bf7623a59e6d8ea6b4a4ee",
+    "url": "/static/media/tomato.2483fde6.png"
+  },
+  {
+    "revision": "b18ebd43df873062f7c388d4990ae370",
+    "url": "/static/media/town-houses.b18ebd43.png"
+  },
+  {
+    "revision": "1a3bfc8457e8eb9a5f7820f7c72c8903",
+    "url": "/static/media/tractor.1a3bfc84.png"
+  },
+  {
+    "revision": "dc5963cb1f652cf16da5522524a77877",
+    "url": "/static/media/trash.dc5963cb.wav"
+  },
+  {
+    "revision": "def8155b4801c00bdd0a61b3e2c5ac0e",
+    "url": "/static/media/up-joy.def8155b.png"
+  },
+  {
+    "revision": "aa904cecebde87e8fba53b5648d67172",
+    "url": "/static/media/upcycle.aa904cec.png"
+  },
+  {
+    "revision": "b79653ad7a2b9735a7b1f3bc13a505bf",
+    "url": "/static/media/wholesale.b79653ad.png"
+  },
+  {
+    "revision": "f80e318213ea3cfce348b9405fdda823",
+    "url": "/static/media/windfarm.f80e3182.png"
+  },
+  {
+    "revision": "2ff68f6bf1c30bd1e47ba4a25ba6821e",
+    "url": "/static/media/wood-cutlery.2ff68f6b.png"
+  }
+]);
